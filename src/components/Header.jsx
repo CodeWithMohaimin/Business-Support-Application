e1d455b3f48b13b0cc7cff16c5b8296d91559ae0
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-03 12:20:00 +0600

    creating a project with tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1>Mohaimin</h1>
+      <div className="navbar w-4/5 bg-gray-200 m-auto flex justify-between items-center">
+        <div className="navLogo text-4xl font-bold text-black">
+          Mohaimin
+        </div>
+        <div className="navLink ">
+          <a className=' p-3' href="#Home">Home</a>
+          <a href="#Home">Find a doctor</a>
+          <a href="#Home">Apps</a>
+          <a href="#Home">Testimonials</a>
+          <a href="#Home">About us</a>
+        </div>
+      </div>
     </div>
   )
 }
